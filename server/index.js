ff91c26644d7d21867793847f36ca82277643abf
patriccormac <socolov1897@gmail.com>
2021-05-12 12:56:54 +0500

    create models

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('./pg-link');
+const sequelize = require('./db');
+const models = require('./models/models');
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,7 +11,7 @@ const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-    
+
     app.listen(PORT, () => {
       console.log(`Server started on port ${PORT}`);
     });
