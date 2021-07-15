3fefb17c707b6528ac3342ad63ce305bbfe7686b
patriccormac <socolov1897@gmail.com>
2021-07-16 00:48:57 +0500

    adding devices, saving static

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,22 @@
+const uuid = require('uuid');
+const path = require('path');
+const { Device } = require('../models/models');
+const ApiError = require('../error/ApiError');
+
 class DeviceController {
-  async addDevice(req, res) {
-    const { name, price, brandId, typeId, info } = req.body;
+  async addDevice(req, res, next) {
+    try {
+      const { name, price, brandId, typeId, info } = req.body;
+      const { img } = req.files;
+      let fileName = uuid.v4() + ".jpg";
+      img.mv(path.resolve(__dirname, '..', 'static', fileName));
+
+      const device = await Device.create({ name, price, brandId, typeId, img: fileName });
+
+      return res.json(device);
+    } catch(e) {
+      next(ApiError.badRequest(e.message));
+    }
   };
 
   async getAllDevices(req, res) {
