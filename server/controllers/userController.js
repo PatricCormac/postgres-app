8580058c71064a30e9737a764b7c5e345eba9ed8
patriccormac <socolov1897@gmail.com>
2021-07-21 09:44:44 +0500

    user login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -29,16 +29,25 @@ class UserController {
     return res.json({ token });
   }
 
-  async login(req, res) {
-    
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({where: { email }});
+
+    if (!user) {
+      return next(ApiError.internal('Пользователь не найден'));
+    }
+
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal('Неверный пароль'));
+    }
+
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({token});
   }
 
   async check(req, res, next) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest('Не задан ID'));
-    }
-    res.json(id)
+    
   }
 }
 
