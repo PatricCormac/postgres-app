f3110053f1511a76497ce2d5b12386c70f88c1fd
patriccormac <socolov1897@gmail.com>
2021-08-01 21:12:17 +0500

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRouter } from "./components/AppRouter";
+
 function App() {
   return (
-    <div>
-      Working
-    </div>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/utils/consts.js
@@ -0,0 +1,6 @@
+export const ADMIN_ROUTE = '/admin';
+export const LOGIN_ROUTE = '/login';
+export const REGISTRATION_ROUTE = '/registration';
+export const SHOP_ROUTE = '/';
+export const DEVICE_ROUTE = '/device';
+export const BASKET_ROUTE = '/basket';
